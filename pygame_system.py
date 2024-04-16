448398046608a5b757ed2f1a3b2b0ef335575bb3
C0B23124 <c0b23124ff@edu.teu.ac.jp>
2024-04-16 15:43:50 +0900

    本日2回目の変更

--- pygame_system.py
+++ pygame_system.py
@@ -9,10 +9,10 @@ def main():
     pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
-    font = pg.font.Font(None, 80)
+    font = pg.font.Font(None, 300)
 
     enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(enn, (255, 255, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
@@ -23,10 +23,10 @@ def main():
         txt = font.render(str(tmr), True, (255, 255, 255))
         screen.fill((50, 50, 50))
         screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
+        screen.blit(enn, [400, 100])
         pg.display.update()
         tmr += 1        
-        clock.tick(1)
+        clock.tick(2)
 
 
 if __name__ == "__main__":
