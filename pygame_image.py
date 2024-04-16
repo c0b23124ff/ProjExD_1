679ac87d603b655c20ff847c55745679e34708b2
C0B23124 <c0b23124ff@edu.teu.ac.jp>
2024-04-16 18:14:00 +0900

    練習1~8

--- pygame_image.py
+++ pygame_image.py
@@ -10,43 +10,38 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png") #練習2
-    kk_img = pg.transform.flip(kk_img, True, False)
-    bg2_img = pg.transform.flip(bg_img, True, False)
-    kk_rct = kk_img.get_rect() #kk_imgを抽出
-    kk_rct.center = 300,200
-
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習７-１
+    kk_img = pg.image.load("fig/3.png")  # 練習２
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習２
+    kk_rct = kk_img.get_rect()  # 練習８-１（こうかとんRectを抽出）
+    kk_rct.center =  300, 200  # 練習８-２（中心座標の設定）
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst=pg.key.get_pressed()
-        x = 0
-        y = 0
+        key_lst = pg.key.get_pressed()
+        # print(key_lst)
         if key_lst[pg.K_UP]:
-            y -= 1
+            # print("上押された")
+            kk_rct.move_ip([0, -1])
         if key_lst[pg.K_DOWN]:
-            y += 1
+            kk_rct.move_ip([0, +1])            
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip([-1, 0])            
         if key_lst[pg.K_RIGHT]:
-            x += 2
-        #if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip([+1, 0])
 
-        kk_rct.move_ip([x-1,y])
+        x = tmr%3200
+        # print(tmr, x)
+        screen.blit(bg_img, [-x, 0])  # 練習６
+        screen.blit(bg_img2, [-x+1600, 0])  # 練習７-１
+        screen.blit(bg_img, [-x+3200, 0])  # 練習７-２
+        screen.blit(bg_img2, [-x+4800, 0])  # 練習７-２
 
-        x = tmr % 3200
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg2_img, [-x+1600, 0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg2_img, [-x+4800, 0])
-
-
-        screen.blit(kk_img,kk_rct)
+        screen.blit(kk_img, kk_rct)  # 練習４
         pg.display.update()
-        tmr += 1 
-       
-        clock.tick(200)
-        
-        
+        tmr += 1        
+        clock.tick(200)  # 練習５
 
 
 if __name__ == "__main__":
