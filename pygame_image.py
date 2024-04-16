f5972cfb6ec5288aca6d64129b7161f121071d08
C0B23124 <c0b23124ff@edu.teu.ac.jp>
2024-04-16 17:33:47 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -26,9 +26,9 @@ def main():
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip([0,1])
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([1,0])
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip([-1,0])
+            kk_rct.move_ip([2,0])
+        #if key_lst[pg.K_LEFT]:
+            #kk_rct.move_ip([-1,0])
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
@@ -39,7 +39,9 @@ def main():
 
         screen.blit(kk_img,kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr += 1 
+        kk_rct.move_ip([-1,0])
+       
         clock.tick(200)
         
         
